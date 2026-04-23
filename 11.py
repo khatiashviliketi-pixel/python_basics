ff2717f40f52acd405976c5d1a2eeab1929e20c3
Keti <khatiashviliketi@gmail.com>
2026-04-23 16:59:44 +0400

    23.04.2026 სამსახურში

---
+++ 11.py
@@ -0,0 +1,20 @@
+
+
+def GreetUser(username):
+    print("Hello, "+username + "! Welcome to the Python course.")
+
+GreetUser("John")
+
+
+#************************************************************
+
+def GreetUser(username):
+    print("Hello, "+username + "! Welcome to the Python course.")
+
+username= "John"
+GreetUser(username)
+
+
+
+#*******************************************************************
+# self გამოიყენება მხოლოდ კლასის შიგნით არსებულ ფუნქციებში
---
+++ 12.py
@@ -0,0 +1,20 @@
+
+
+def CalculateAverage(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+print("The average of 10, 20, and 30 is", CalculateAverage(10, 20, 30))
+
+print("**********************************************************")
+
+# ეს მთლად სწორი არაა მგონი, რიცხვები ფუნქციის გარეთ როა იგივეს არ ნიშნავს რასაც ფუნქციის შიგნით
+def CalculateAverage(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+num1 = 10
+num2 = 20
+num3 = 30
+
+print("The average of 10, 20, and 30 is", CalculateAverage(num1, num2, num3))
+
+print("**********************************************************")

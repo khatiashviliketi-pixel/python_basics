f8467a974b6f124662f3ff65b4bea95c787eb85b
Keti <khatiashviliketi@gmail.com>
2026-05-06 18:33:32 +0400

    6.05.2026 სამსახურში

--- 14.py
+++ 14.py
@@ -1,3 +1,16 @@
 
-with open('file1.txt') as file:
+lineCount = 0
+with open('file1.txt', 'r') as file:
+    for line in file.readlines():  #for _ in file.readlines(): line როცა არ გვჭირდება სიტყვა ესეც წერენო
+        lineCount = lineCount + 1
+    print("Total number of lines:", lineCount)
+
+
+
+#იგივე ლოგიკაა რაც "loops" ფაილში მაქვს ნასწავლი
+#summation = 0
+#for j in range(1, 6):
+#    summation = summation + j
+#print(summation)
+
 
